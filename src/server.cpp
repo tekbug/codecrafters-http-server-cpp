e97a08a4e525e56180695189ae7afe18675a32bf
tekbug <minimum.asparagus@gmail.com>
2024-07-09 10:41:50 +0300

    Extract URL and return 404

--- src/server.cpp
+++ src/server.cpp
@@ -46,12 +46,33 @@ int main(int argc, char **argv) {
 
   std::cout << "Waiting for a client to connect...\n";
 
+  // Respond with 200
   int client = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+
   std::string message = "HTTP/1.1 200 OK\r\n\r\n";
-  send(client, message.c_str(), message.length(), 0);
-  std::cout << "Client connected\n";
+
+  int sendMsg = send(client, message.c_str(), message.length(), 0);
+
+  if(sendMsg < 0) {
+    std::cerr << "Error occured";
+  } else {
+    std::cout << message;
+  }
+
+  // extract URL path
+  std::string client_message(1024, '\0');
+
+  ssize_t accept = recv(client, (void *)&client_message[0], client_message.max_size(), 0);
+
+  if(accept < 0) {
+    std::cout << "Client message is short";
+    close(client);
+    close(server_fd);
+    return 1;
+  }
+
+  std::string response = client_message.starts_with("GET / HTTP/1.1\r\n") ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 
   close(server_fd);
-
   return 0;
 }
