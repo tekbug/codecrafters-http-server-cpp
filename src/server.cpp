f27284ccf2b8262df723e57d9374104098bc004e
tekbug <minimum.asparagus@gmail.com>
2024-07-30 00:52:49 +0300

    GZIP compression

--- src/server.cpp
+++ src/server.cpp
@@ -10,6 +10,7 @@
 #include <netdb.h>
 #include <thread>
 #include <vector>
+#include <zlib.h>
 #include <fstream>
 
 /*
@@ -18,6 +19,54 @@
  * By tekbug
  */
 
+
+std::string compress_string(const std::string& str, int compressionlevel = Z_BEST_COMPRESSION) {
+
+    z_stream zs;
+
+    memset(&zs, 0, sizeof(zs));
+
+    if (deflateInit2(&zs, compressionlevel, Z_DEFLATED, 31, 8, Z_DEFAULT_STRATEGY) != Z_OK)
+
+        throw(std::runtime_error("deflateInit failed while compressing."));
+
+    zs.next_in = (Bytef*)str.data();
+
+    zs.avail_in = str.size();
+
+    int ret;
+
+    char outbuffer[32768];
+
+    std::string outstring;
+
+    do {
+
+        zs.next_out = reinterpret_cast<Bytef*>(outbuffer);
+
+        zs.avail_out = sizeof(outbuffer);
+
+        ret = deflate(&zs, Z_FINISH);
+
+        if (outstring.size() < zs.total_out) {
+
+            outstring.append(outbuffer, zs.total_out - outstring.size());
+
+        }
+
+    } while (ret == Z_OK);
+
+    deflateEnd(&zs);
+
+    if (ret != Z_STREAM_END) {
+
+        throw(std::runtime_error("Exception during zlib compression: " + std::to_string(ret)));
+
+    }
+
+    return outstring;
+
+}
 
 
 void concurrent_users(int client, const std::string &dir) {
@@ -77,7 +126,8 @@ void concurrent_users(int client, const std::string &dir) {
       }
     }
     if(zipSupport) {
-      std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " + std::to_string(req_path.size()) + "\r\n\r\n" + req_path;
+      std::string compressRes = compress_string(req_path);
+      std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip\r\nContent-Length: " + std::to_string(compressRes.size()) + "\r\n\r\n" + compressRes;
       bsend = send(client, response.c_str(), response.size(), 0);
     } else {
       std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + std::to_string(req_path.size()) + "\r\n\r\n" + req_path;
